2ce4efa437fc8d066d83b4158ec778b656948241
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-10-06 15:57:23 -0500

    added MUI styling and CSS styling for the home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,70 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import MovieList from '../MovieList/MovieList'
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import {
+  createTheme,
+  ThemeProvider,
+  Container,
+  Typography,
+  AppBar,
+  Toolbar,
+  Button,
+} from "@mui/material";
+import MovieList from "../MovieList/MovieList";
+import "./App.css";
+// import MovieDetails from '../MovieDetails/MovieDetails'; doesn't exist
+
+const theme = createTheme({
+  palette: {
+    mode: "dark", // Enables dark mode
+    primary: {
+      main: "#ffffff", // White color
+    },
+    secondary: {
+      main: "#000000", // Black color
+    },
+  },
+  typography: {
+    fontFamily: "Arial, sans-serif",
+    h1: {
+      fontSize: "2rem",
+      fontWeight: "bold",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        
-        {/* Details page */}
-
-        {/* Add Movie page */}
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className="App">
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h1" component="div" style={{ flexGrow: 1 }}>
+                The Movies Saga!
+              </Typography>
+              {/* <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/add-movie">
+                Add Movie
+              </Button> */}
+            </Toolbar>
+          </AppBar>
+          <Container>
+            <Route path="/" exact>
+              <MovieList />
+            </Route>
+            {/* <Route path="/details/:id">
+              <MovieDetails />
+            </Route>
+            <Route path="/add-movie">
+              <AddMovie />
+            </Route> */}
+          </Container>
+        </div>
       </Router>
-    </div>
+    </ThemeProvider>
   );
 }
-
 
 export default App;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,40 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import "./MovieList.css";
 
 function MovieList() {
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movies);
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" });
+  }, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+  return (
+    <main>
+      <Typography className="movie-header" variant="h2" gutterBottom>
+        MovieList
+      </Typography>
+      <section className="movies">
+        {movies.map((movie) => (
+          <Card key={movie.id} className="movie-card">
+            <CardMedia
+              component="img"
+              height="300"
+              image={movie.poster}
+              alt={movie.title}
+            />
+            <CardContent>
+              <Typography variant="h6" component="div">
+                {movie.title}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default MovieList;
